e0bb6e76f9cc896305d916402686296cccf277a5
Jason Scholl <jason.e.scholl@gmail.com>
2017-11-06 11:21:38 -0800

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mike.schuette@gmail.com'
 license          'FreeBSD'
 description      'Installs and configures AWS CloudWatch Logs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.5'
+version          '0.0.6'
 source_url       'https://github.com/MikeSchuette/chef-cwlogs'
 issues_url       'https://github.com/MikeSchuette/chef-cwlogs/issues'
 
